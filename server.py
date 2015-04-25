652b894549896c884271574e78ed9edaec053e7c
minhoryang <minhoryang@gmail.com>
2015-04-26 00:08:24 +0900

    NewRelic Service Added.

--- server.py
+++ server.py
@@ -61,4 +61,6 @@ api.add_resource(RegisterApi, '/users')
 
 if __name__ == "__main__":
     init_db()
+    import newrelic.agent
+    newrelic.agent.initialize('newrelic.ini')
     app.run('0.0.0.0', port=5000, debug=True)
