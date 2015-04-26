61f5506e84133f1ce07d11bc04cce657b3f4e9d7
minhoryang <minhoryang@gmail.com>
2015-04-26 14:28:14 +0900

    Record Model.

--- application/__init__.py
+++ application/__init__.py
@@ -7,10 +7,11 @@ def create_app():
 	app.config['PROJECT_PATH'] = abspath(join(dirname(__file__), '..'))
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/db/app.db' % (app.config['PROJECT_PATH'], )
 	
-	from .models import db, user
+	from .models import db, user, record
 	db.init_app(app)
 
-	api = Api(app, version='1.0', title='MarryBird API', description='serve JSON')
+	api = Api(app, version='1.0', title='MarryBird API', description='Hi There!')
 	user.init(api)
+	record.init(api)
 
 	return app
