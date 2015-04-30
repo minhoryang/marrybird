3522c5f5d233791e924f46626a80a7aaf68650c0
Minho Ryang <minhoryang@gmail.com>
2015-04-30 22:41:25 +0900

    Space Changing... :( Not fitted with PyCharm.

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,19 @@
 from os.path import abspath, dirname, join
+
 from flask import Flask
-from flask.ext.restplus import Api, Resource, fields
+from flask.ext.restplus import Api
+
 
 def create_app():
-	app = Flask(__name__)
-	app.config['PROJECT_PATH'] = abspath(join(dirname(__file__), '..'))
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/db/app.db' % (app.config['PROJECT_PATH'], )
-	
-	from .models import db, user, record
-	db.init_app(app)
+    app = Flask(__name__)
+    app.config['PROJECT_PATH'] = abspath(join(dirname(__file__), '..'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/db/app.db' % (app.config['PROJECT_PATH'], )
 
-	api = Api(app, version='1.0', title='MarryBird API', description='Hi There!')
-	user.init(api)
-	record.init(api)
+    from .models import db, user, record
+    db.init_app(app)
 
-	return app
+    api = Api(app, version='1.0', title='MarryBird API', description='Hi There!')
+    user.init(api)
+    record.init(api)
+
+    return app
--- application/models/record.py
+++ application/models/record.py
@@ -1,81 +1,85 @@
 """handles the user's records."""
 from datetime import datetime
 
-from flask.ext.restplus import Resource, fields
-from sqlalchemy.exc import IntegrityError
-
+from flask.ext.restplus import Resource
 from ._base import db
 from .user import User
 
 class Record(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	created_at = db.Column(db.DateTime, default=datetime.now)
-	modified_at = db.Column(db.DateTime, default=datetime.now)
-	username = db.Column(db.String(50), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    modified_at = db.Column(db.DateTime, default=datetime.now)
+    username = db.Column(db.String(50), unique=True)
 
-	height = db.Column(db.String(50))
-	is_disabled = db.Column(db.String(50))
-	is_smoker = db.Column(db.String(50))
-	is_drinker = db.Column(db.String(50))
-	has_habits = db.Column(db.String(50))
-	has_religion = db.Column(db.String(50))
-	mbti = db.Column(db.String(10))
-	has_car = db.Column(db.String(50))
-	has_home = db.Column(db.String(50))
-	military = db.Column(db.String(50))
-	has_office = db.Column(db.String(50))
-	has_office_title = db.Column(db.String(50))
-	has_office_salary = db.Column(db.String(50))
-	graduated_school = db.Column(db.String(50))
-	photo_url = db.Column(db.String(50))
+    height = db.Column(db.String(50))
+    is_disabled = db.Column(db.String(50))
+    is_smoker = db.Column(db.String(50))
+    is_drinker = db.Column(db.String(50))
+    has_habits = db.Column(db.String(50))
+    has_religion = db.Column(db.String(50))
+    mbti = db.Column(db.String(10))
+    has_car = db.Column(db.String(50))
+    has_home = db.Column(db.String(50))
+    military = db.Column(db.String(50))
+    has_office = db.Column(db.String(50))
+    has_office_title = db.Column(db.String(50))
+    has_office_salary = db.Column(db.String(50))
+    graduated_school = db.Column(db.String(50))
+    photo_url = db.Column(db.String(50))
 
-	def __setattr__(self, key, value):
-		super(Record, self).__setattr__(key, value)
-		#super(Record, self).__setattr__('modified_at', datetime.now)
+    def __setattr__(self, key, value):
+        super(Record, self).__setattr__(key, value)
+    #super(Record, self).__setattr__('modified_at', datetime.now)
 
 def init(api):
-	namespace = api.namespace(__name__.split('.')[-1], description=__doc__)
-	keywords = list()
-	for i in Record.__dict__.keys():
-		if i[0] == '_':
-			pass
-		elif i in ['id', 'created_at', 'modified_at', 'username']:
-			pass
-		else:
-			keywords.append(i)
-	keywords.sort()
+    namespace = api.namespace(__name__.split('.')[-1], description=__doc__)
+    keywords = list()
+    for i in Record.__dict__.keys():
+        if i[0] == '_':
+            pass
+        elif i in ['id', 'created_at', 'modified_at', 'username']:
+            pass
+        else:
+            keywords.append(i)
+    keywords.sort()
 
-	@namespace.route('/')
-	class AvailableKeywords(Resource):
-		def get(self):
-			"""Available Keyword List."""
-			return {"keywords": keywords}
+    @namespace.route('/')
+    class AvailableKeywords(Resource):
+        def get(self):
+            """Available Keyword List."""
+            return {"keywords": keywords}
 
-	@namespace.route('/<string:username>')
-	class GetUsersKeywordsStatus(Resource):
-		def get(self, username):
-			"""Get User's Keywords Status (Inputted or not, then which)."""
-			return {'status': 'User Not Found'}, 404
+    @namespace.route('/<string:username>')
+    class GetUsersKeywordsStatus(Resource):
+        def get(self, username):
+            """Get User's Keywords Status (Inputted or not, then which)."""
+            return {'status': 'User Not Found'}, 404
 
-	@namespace.route('/<string:username>/<string:keyword>')
-	class UsersKeywords(Resource):
-		def get(self, username, keyword):
-			"""Get User's Keyword Content."""
-			rec = Record.query.filter(Record.username == username).first()
-			if rec:
-				return rec.__dict__[keyword]
-			else:
-				return {'status': 'Not Found'}, 404
-		def post(self, username, keyword):
-			"""Set User's Keyword Content."""
-			usr = User.query.filter(User.username == username).first()
-			if usr:
-				rec = Record.query.filter(Record.username == username).first()
-				if not rec:
-					rec = Record(username=username)
-				rec.__dict__[keyword] = keyword
-				db.session.add(rec)
-				db.session.commit()
-				return {'status': 'done'}
-			else:
-				return {'status': 'Not Found'}, 404
+    @namespace.route('/<string:username>/<string:keyword>')
+    class UsersKeywords(Resource):
+        def get(self, username, keyword):
+            """Get User's Keyword Content."""
+            rec = Record.query.filter(Record.username == username).first()
+            if rec:
+                return rec.__dict__[keyword]
+            else:
+                return {'status': 'Not Found'}, 404
+
+        wanted = api.parser()
+        wanted.add_argument('value', type=str, required=True, help='{"value": ""}', location='json')
+
+        @api.doc(parser=wanted)
+        def post(self, username, keyword):
+            """Set User's Keyword Content."""
+            self.wanted.parse_args()
+            usr = User.query.filter(User.username == username).first()
+            if usr:
+                rec = Record.query.filter(Record.username == username).first()
+                if not rec:
+                    rec = Record(username=username)
+                rec.__dict__[keyword] = args['value']
+                db.session.add(rec)
+                db.session.commit()
+                return {'status': 'done'}
+            else:
+                return {'status': 'Not Found'}, 404
