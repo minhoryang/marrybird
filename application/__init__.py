e6df7ea3b12c18738dd24aeb33b91a29c3c87204
Minho Ryang <minhoryang@gmail.com>
2015-05-02 17:07:20 +0900

    [MBIRD-1] Add Admin Page (api.reluv.me/admin) to see DB

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,22 @@
 from os.path import abspath, dirname, join
 
 from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 from flask.ext.restplus import Api
-
 
 def create_app():
     app = Flask(__name__)
     app.config['PROJECT_PATH'] = abspath(join(dirname(__file__), '..'))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s/db/app.db' % (app.config['PROJECT_PATH'], )
+    app.config['secret_key'] = 'developer'  # TODO: need to change.
 
     from .models import db, user, record
     db.init_app(app)
+
+    admin = Admin(app)
+    admin.add_view(ModelView(user.User, db.session))
+    admin.add_view(ModelView(record.Record, db.session))
 
     api = Api(app, version='1.0', title='MarryBird API', description='Hi There!')
     user.init(api)
