c316bce411de192263338df38afdd4ebf9507724
Minho Ryang <minhoryang@gmail.com>
2015-07-09 20:46:32 +0900

    Add TODO at external/slack.py

--- application/externals/slack.py
+++ application/externals/slack.py
@@ -4,6 +4,7 @@ import json
 
 WEB_HOOK_URL = "https://hooks.slack.com/services/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"
 
+# TODO : TOO LONG TO SYNCRONOUS. SO NEED TO PULL THIS ASYNC HEAVEN LIKE celery.
 def push(message="", channel="#progress"):
     payload = {
         "username": "Server.py",
