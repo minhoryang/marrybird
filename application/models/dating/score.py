3f48716a903aee08e651319ba834aacdfad5a43d
Minho Ryang <minhoryang@gmail.com>
2015-08-09 16:03:04 +0900

    Score 5%

--- application/models/dating/score.py
+++ application/models/dating/score.py
@@ -9,6 +9,11 @@ from .. import db
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+
+    school_score = db.Column(db.Float, nullable=False)
+    work_score = db.Column(db.Float, nullable=False)
+    attraction_score = db.Column(db.Float, nullable=False)
 
 def init(api, jwt):
     pass  # XXX: No plan
