a18c624de06474d1eb9625432dfd1855fc22d572
Minho Ryang <minhoryang@gmail.com>
2015-08-24 02:28:07 +0900

    Revert "Remove sqlalchemy_enum34 and Replace it to sqlalchemy_utils.types.choice"
    This reverts commit bb3ef12bc8dee1bd7a641430b96305bc40f79a96.

--- application/models/dating/met.py
+++ application/models/dating/met.py
@@ -6,7 +6,7 @@ from enum import Enum
 from datetime import datetime
 
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy_utils.types.choice import ChoiceType
+from sqlalchemy_enum34 import EnumType
 
 from .. import db
 
@@ -22,7 +22,7 @@ class Met(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     response_id = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime.now)
-    type = db.Column(ChoiceType(MetType), nullable=False)
+    type = db.Column(EnumType(MetType), nullable=False)
     A = db.Column(db.String(50))
     B = db.Column(db.String(50))
 
@@ -38,7 +38,7 @@ class Met_Rejected(Met):
     def create(cls, response_id, A, B):
         c = cls()
         c.response_id = response_id
-        c.type = MetType.rejected.value
+        c.type = MetType.rejected
         c.A = A
         c.B = B
         return c
@@ -51,7 +51,7 @@ class Met_Accepted(Met):
     def create(cls, response_id, A, B):
         c = cls()
         c.response_id = response_id
-        c.type = MetType.accepted.value
+        c.type = MetType.accepted
         c.A = A
         c.B = B
         return c
@@ -64,7 +64,7 @@ class Met_NotResponsed(Met):
     def create(cls, response_id, A, B):
         c = cls()
         c.response_id = response_id
-        c.type = MetType.notresponsed.value
+        c.type = MetType.notresponsed
         c.A = A
         c.B = B
         return c
--- application/models/dating/score.py
+++ application/models/dating/score.py
@@ -1,7 +1,7 @@
 """."""
 __author__ = 'minhoryang'
 
-from sqlalchemy_utils.types.choice import ChoiceType
+from sqlalchemy_enum34 import EnumType
 
 from .. import db
 from .tier import TierType
@@ -19,13 +19,13 @@ class Score(db.Model):
     attraction_score = db.Column(db.Float, nullable=False)
 
     tier = db.Column(db.String(2), nullable=False)
-    tierr = db.Column(ChoiceType(TierType))
+    tierr = db.Column(EnumType(TierType))
 
     def __setattr__(self, key, value):
         #old = self.tierr
         super(Score, self).__setattr__(key, value)
         if key is 'tier':
-            self.tierr = value
+            self.tierr = TierType(value)
 
 
 def init(api, jwt):
