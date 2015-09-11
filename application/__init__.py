adc6159c198840c1829a477ff9c7737729a1b804
Minho Ryang <minhoryang@gmail.com>
2015-09-11 19:07:16 +0900

    Celery Welcome!

--- application/__init__.py
+++ application/__init__.py
@@ -45,3 +45,20 @@ def create_app(isolated=False):
         MyJWT.Bridger(plugins['jwt'])
 
     return app
+
+
+def create_celery(app=None):
+    app = app or create_app(isolated=True)
+
+    celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+
+    return celery
