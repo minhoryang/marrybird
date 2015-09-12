e35ce1147dcd539779aa1f968fdf588cad0cde96
Minho Ryang <minhoryang@gmail.com>
2015-09-12 21:32:41 +0900

    [MBIRD-96] Resolved flask-restplus url_for my way.

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,9 @@ from os.path import abspath, dirname, join
 from flask import Flask
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
-from flask.ext.restplus import Api
 
 from .models import ENABLE_MODELS, db
+from .utils.my_api import MyApi
 from .utils.my_jwt import MyJWT
 from .utils.constant import *
 
@@ -23,7 +23,7 @@ def create_app():
     app.config['SQLALCHEMY_BINDS'] = {}
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
-    api = Api(app, version='1.1', title='MarryBird API', description='Hi There!')
+    api = MyApi(app, version='1.1', title='MarryBird API', description='Hi There!')
     jwt = MyJWT(app)
     admin = Admin(app, name="admin")
     db.app = app  # XXX : FIXED DB Context Issue without launching the app.
