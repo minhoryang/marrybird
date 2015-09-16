ff5344cf34ffcc9d67bab47441314f3677aad5d0
Minho Ryang <minhoryang@gmail.com>
2015-09-16 17:44:51 +0900

    [BUGFIX] DB Commit First before Celery Run.

--- application/tasks/phone.py
+++ application/tasks/phone.py
@@ -7,19 +7,21 @@ Celery = create_celery()
 
 
 @Celery.task(bind=True, ignore_result=True)
-def PhoneCheckRequest_post(self):
+def PhoneCheckRequest_post(self, phone, status):
     from .. import db
     from ..externals.slack import push
     from ..models.phone import Phone
 
     found = Phone.query.filter(
-        Phone.celery_id == str(self.request.id)
+        Phone.phone == phone,
+        Phone.status == status,
     ).first()
 
     # TODO: Need to connect externals/phonenum-check-by-company
     push('휴대폰인증)' + found.phone + ' 로 ' + found.status + ' 를 보내주세요.')
 
     found.celery_status = 'sent'
+    found.celery_id = str(self.request.id)
     db.session.add(found)
     db.session.commit()
 
