efc49941baa98a241a73a87613b3d2664fee0b25
Minho Ryang <minhoryang@gmail.com>
2015-09-16 17:48:47 +0900

    [MERGED]ToMarrybird Fix/DBMigrate

--- application/models/tomarrybird.py
+++ application/models/tomarrybird.py
@@ -20,7 +20,9 @@ class ToMarrybird(db.Model):
     message = db.Column(db.String(200))
 
 
-def init(api, jwt):
+def init(**kwargs):
+    api = kwargs['api']
+    jwt = kwargs['jwt']
     namespace = api.namespace(__name__.split('.')[-1], description=__doc__.split('.')[0])
     authorization = api.parser()
     authorization.add_argument('authorization', type=str, required=True, help='"Bearer $JsonWebToken', location='headers')
