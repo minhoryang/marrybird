0248e0b8eb6a4ba4b59e862e14bf30d67f62b9da
Minho Ryang <minhoryang@gmail.com>
2015-09-21 23:49:18 +0900

    Welcome SQLAlchemy + PostgreSQL

--- application/utils/constant.py
+++ application/utils/constant.py
@@ -1,4 +1,6 @@
 __author__ = 'minhoryang'
 
-SQLALCHEMY_DATABASE_URI = lambda app, name: 'sqlite:///%s/db/%s.db' % (app.config['PROJECT_PATH'], name)
+# SQLALCHEMY_DATABASE_URI = lambda app, name: 'sqlite:///%s/db/%s.db' % (app.config['PROJECT_PATH'], name)
+# SQLALCHEMY_BINDS_RULES = lambda app, name: app.config['SQLALCHEMY_BINDS'].update({name: SQLALCHEMY_DATABASE_URI(app, name)})
+SQLALCHEMY_DATABASE_URI = lambda app, name: 'postgresql://marrybird:marrybird-localhost@127.0.0.1/%s' % (name, )
 SQLALCHEMY_BINDS_RULES = lambda app, name: app.config['SQLALCHEMY_BINDS'].update({name: SQLALCHEMY_DATABASE_URI(app, name)})
