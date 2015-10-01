81ddc38f7041629e07c552e63cb2a8321f2aca9c
Minho Ryang <minhoryang@gmail.com>
2015-10-02 08:54:24 +0900

    [BUGFIX] phone celery failed sometime - fixed.

--- application/tasks/phone.py
+++ application/tasks/phone.py
@@ -6,7 +6,7 @@ Celery = create_celery()
 
 
 @Celery.task(bind=True, ignore_result=True)
-def PhoneCheckRequest_post(self, phone, status):
+def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
     from .. import db
     from ..externals.slack import push
     from ..models.phone import Phone
@@ -15,6 +15,12 @@ def PhoneCheckRequest_post(self, phone, status):
         Phone.phone == phone,
         Phone.status == status,
     ).first()
+    if not found:
+        if retries > max_retries:
+            return 'failed - no record'
+        if 'celery' in Celery.conf.MARRYBIRD_FLAGS:
+            return 'retry - %s' % (PhoneCheckRequest_post.delay(phone, status, retires+1),)
+        return PhoneCheckRequest_post(self, phone, status, retries+1)
 
     # TODO: Need to connect externals/phonenum-check-by-company
     push('휴대폰인증)' + found.phone + ' 로 ' + found.status + ' 를 보내주세요.')
@@ -24,4 +30,4 @@ def PhoneCheckRequest_post(self, phone, status):
         found.celery_id = str(self.request.id)
     db.session.add(found)
     db.session.commit()
-    return ret
+    return (phone, status, retries)
