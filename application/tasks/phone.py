612a3d5faa2318e867d5cc2d726d726bf829b0b4
Minho Ryang <minhoryang@gmail.com>
2015-11-09 13:13:00 +0900

    APIStore SMS

--- application/tasks/phone.py
+++ application/tasks/phone.py
@@ -8,7 +8,7 @@ Celery = create_celery()
 @Celery.task(bind=True, ignore_result=True)
 def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
     from .. import db
-    from ..externals.slack import push
+    from ..externals.apistore import push
     from ..models.phone import Phone
 
     found = Phone.query.filter(
@@ -22,8 +22,10 @@ def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
             return 'retry - %s' % (PhoneCheckRequest_post.delay(phone, status, retries+1),)
         return PhoneCheckRequest_post(self, phone, status, retries+1)
 
-    # TODO: Need to connect externals/phonenum-check-by-company
-    push('휴대폰인증)' + found.phone + ' 로 ' + found.status + ' 를 보내주세요.')
+    # XXX: connected externals/phonenum-check-by-company
+    push(found.status, found.phone)
+
+    # TODO: Recheck "is sent already?" then ignore it.
 
     found.celery_status = 'sent'
     if self.request.id:
