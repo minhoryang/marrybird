69348a5cbce9e06163d141faf3aed9429ef549ae
Minho Ryang <minhoryang@gmail.com>
2015-11-09 14:42:39 +0900

    COOLSMS!!!! SO COOL!

--- application/tasks/phone.py
+++ application/tasks/phone.py
@@ -8,7 +8,7 @@ Celery = create_celery()
 @Celery.task(bind=True, ignore_result=True)
 def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
     from .. import db
-    from ..externals.apistore import push
+    from ..externals.coolsms_ import push
     from ..models.phone import Phone
 
     found = Phone.query.filter(
@@ -23,7 +23,7 @@ def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
         return PhoneCheckRequest_post(self, phone, status, retries+1)
 
     # XXX: connected externals/phonenum-check-by-company
-    push(found.status, found.phone)
+    out = push(found.status, found.phone)
 
     # TODO: Recheck "is sent already?" then ignore it.
 
@@ -32,4 +32,4 @@ def PhoneCheckRequest_post(self, phone, status, retries=0, max_retries=10):
         found.celery_id = str(self.request.id)
     db.session.add(found)
     db.session.commit()
-    return (phone, status, retries)
+    return (phone, status, retries, out)
